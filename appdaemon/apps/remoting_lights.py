87b5c07d74930889dfb5ba01870574d11e04761c
Gerwin Jonker <gwjonker@gmail.com>
2019-09-06 18:00:21 +0200

    AppDeamon: inc/dec brightness lights

--- appdaemon/apps/remoting_lights.py
+++ appdaemon/apps/remoting_lights.py
@@ -14,9 +14,48 @@ class TurnOffLight(hass.Hass):
 class TurnOnLight(hass.Hass):
 
     def initialize(self):
-        self.listen_state(self.turn_on_lights, "sensor.mi_magic_cube", new = "flip90")
+        self.listen_state(self.turn_on_lights, "sensor.mi_magic_cube", new = "slide")
 
     def turn_on_lights(self, entity, attribute, old, new, kwargs):
         for light in self.args["lights"]:
             self.turn_on(light)
             self.log("Turned on light: {}".format(light))
+
+
+class IncreaseBrightnessLight(hass.Hass):
+
+    def initialize(self):
+        self.listen_state(self.inc_brightness_lights, "sensor.mi_magic_cube", new = "flip90")
+
+    def inc_brightness_lights(self, entity, attribute, old, new, kwargs):
+        step = self.args["step"]
+        for light in self.args["lights"]:
+            if self.get_state(light) == "on":
+                cb = self.get_state(light, attribute = "brightness")
+                
+                nb = cb + step
+                if (nb)>253:
+                    nb = 254
+                
+                self.set_state(light, attributes = { "brightness": nb})
+                self.log("brightness light {} is set from brightness {} to {}".format(light, cb, nb))
+
+
+class DecreaseBrightnessLight(hass.Hass):
+
+    def initialize(self):
+        self.listen_state(self.dec_brightness_lights, "sensor.mi_magic_cube", new = "flip180")
+
+    def dec_brightness_lights(self, entity, attribute, old, new, kwargs):
+        step = self.args["step"]
+        for light in self.args["lights"]:
+            if self.get_state(light) == "on":
+                cb = self.get_state(light, attribute = "brightness")
+                
+                nb = cb - step
+                if (nb)<1:
+                    nb = 0
+                
+                self.set_state(light, attributes = { "brightness": nb})
+                self.log("brightness light {} is set from brightness {} to {}".format(light, cb, nb))
+
